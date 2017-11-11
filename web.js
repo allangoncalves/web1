f7217d1ec3aa7314c735f457f30fdf0ebd0ac4af
allangoncalves <allangoncalves@outlook.com>
2017-11-11 11:49:19 -0300

    showing history and quiting

--- web.js
+++ web.js
@@ -6,12 +6,40 @@ TipoJogador = {
 	COMPUTADOR: 1,
 };
 
+
+
 Item = ["PEDRA", "PAPEL", "TESOURA"];
 
 ModoJogo = {
 	PvP: 0,
 	PvC: 1,
 };
+
+modo_atual = ModoJogo.PvC;
+
+$(document).ready(
+	function(){
+		$("#VsPessoa").off().click(
+			function(){
+				$("#TipoJogo").hide();
+				modo_atual = ModoJogo.PvP;
+				criarCadastro();
+			}
+		);
+	}
+)
+
+$(document).ready(
+	function(){
+		$("#VsComputador").off().click(
+			function(){
+				$("#TipoJogo").hide();
+				modo_atual = ModoJogo.PvC;
+				criarCadastro();
+			}
+		);
+	}
+)
 
 function Jogador(nome, sexo, tipoJogador){
 	this.nome = nome;
@@ -39,9 +67,21 @@ function verificarJogador(nome){
 	return null;
 }
 
-function criarJogadores(Jogo){
+function desistir(id) {
+	$("#Jogo").hide();
+	$("#TipoJogo").attr('style', 'display: inherit');
+	if (id == 1){
+
+	}
+	else{
+
+	}
 	
-	if(Jogo == ModoJogo.PvC){
+}
+
+function criarJogadores(){
+	
+	if(modo_atual == ModoJogo.PvC){
 		jog_1_nome = $("input[name='nome1']").val();
 		jog_1_sexo = $("input[name='sexo1']:checked").val();
 
@@ -60,7 +100,7 @@ function criarJogadores(Jogo){
 		j2 = computador;
 	}
 	else{
-		if (Jogo == ModoJogo.PvP){
+		if (modo_atual == ModoJogo.PvP){
 			
 			jog_1_nome = $("input[name='nome1']").val();
 			jog_1_sexo = $("input[name='sexo1']:checked").val();
@@ -98,29 +138,19 @@ function criarJogadores(Jogo){
 	}
 	//AQUI
 	$("#formulario").hide();
-	InterfaceJogo(Jogo)
+	InterfaceJogo(modo_atual)
 }
 
 function criarFormulario(id){
-	$("#formulario").append('<div class="form" id="jogador1"></div>');
-	$("#jogador1").append('<p>Jogador 1</p>');
-	$("#jogador1").append('Nome: <input type="text" name="nome1"> ');
-	$("#jogador1").append('<br> Sexo: ');
-	$("#jogador1").append('<input type="radio" name="sexo1" value="feminino" checked> Feminino');
-	$("#jogador1").append('<input type="radio" name="sexo1" value="masculino"> Masculino <br>');	
+	$("#jogador1").attr('style', 'display: inherit');
 	if(id == 1){
-		$("#formulario").append('<div class="form" id="jogador2"></div>');
-		$("#jogador2").append('<p>Jogador 2</p>');
-		$("#jogador2").append('Nome: <input type="text" name="nome2"> ');
-		$("#jogador2").append('<br> Sexo: ');
-		$("#jogador2").append('<input type="radio" name="sexo2" value="feminino" checked> Feminino');
-		$("#jogador2").append('<input type="radio" name="sexo2" value="masculino"> Masculino <br>');	
+		$("#jogador2").attr('style', 'display: inherit');
 	}
 }
 
-function criarCadastro(id){
-	$("#Cadastro").append('<div id="formulario"></div>');
-	if(ModoJogo.PvP == id){
+function criarCadastro(){
+	$("#formulario").attr('style', 'display: inherit');
+	if(ModoJogo.PvP == modo_atual){
 		criarFormulario(1);
 		$("#formulario").append('<button class="btn btn-primary" id="submitFormulario" type="button" onclick="criarJogadores(ModoJogo.PvP)">Confirmar</button>');
 	}
@@ -131,23 +161,32 @@ function criarCadastro(id){
 	}
 }
 
-function verificarResultado(Jogo, resultado1, resultado2) {
-	if(Jogo == ModoJogo.PvP){
+function verificarResultado(resultado1, resultado2) {
+	if(modo_atual == ModoJogo.PvP){
 		var indice1 = verificarJogador(j1.nome);
 		var indice2 = verificarJogador(j2.nome);
 		if(resultado1 == "PEDRA"){
 			if(resultado2 == resultado1){
 				jogadores[indice1].pontuacao++;
 				jogadores[indice2].pontuacao++;
+				$("#historico").append('<p class="resultado">EMPATE<br>'+
+					j1.nome+'	vs	'+j2.nome+'<br>'+
+					resultado1+' == '+resultado2+'</p>');
 				return 0;
 			}
 			else{
 				if(resultado2 == "PAPEL"){
 					jogadores[indice2].pontuacao += 3;
+					$("#historico").append('<p class="resultado">Jogador 2 ganhou.<br>'+
+						j1.nome+'	vs	'+j2.nome+'<br>'+
+						resultado1+' < '+resultado2+'</p>');
 					return 2;
 				}
 				else{
 					jogadores[indice1].pontuacao += 3;
+					$("#historico").append('<p class="resultado">Jogador 1 ganhou.<br>'+
+						j1.nome+'	vs	'+j2.nome+'<br>'+
+						resultado1+' > '+resultado2+'</p>');
 					return 1;
 				}
 			}
@@ -157,14 +196,23 @@ function verificarResultado(Jogo, resultado1, resultado2) {
 				if(resultado2 == resultado1){
 					jogadores[indice1].pontuacao++;
 					jogadores[indice2].pontuacao++;
+					$("#historico").append('<p class="resultado">EMPATE<br>'+
+						j1.nome+'	vs	'+j2.nome+'<br>'+
+						resultado1+' == '+resultado2+'</p>');
 					return 0;
 				}else{
 					if(resultado2 == "PEDRA"){
 						jogadores[indice1].pontuacao += 3;
+						$("#historico").append('<p class="resultado">Jogador 1 ganhou.<br>'+
+							j1.nome+'	vs	'+j2.nome+'<br>'+
+							resultado1+' > '+resultado2+'</p>');
 						return 1;
 					}
 					else{
 						jogadores[indice2].pontuacao += 3;
+						$("#historico").append('<p class="resultado">Jogador 2 ganhou.<br>'+
+							j1.nome+'	vs	'+j2.nome+'<br>'+
+							resultado1+' < '+resultado2+'</p>');
 						return 2;
 					}
 				}
@@ -174,15 +222,24 @@ function verificarResultado(Jogo, resultado1, resultado2) {
 					if(resultado2 == resultado1){
 						jogadores[indice1].pontuacao++;
 						jogadores[indice2].pontuacao++;
+						$("#historico").append('<p class="resultado">EMPATE<br>'+
+							j1.nome+'	vs	'+j2.nome+'<br>'+
+							resultado1+' == '+resultado2+'</p>');
 						return 0;
 					}
 					else{
 						if(resultado2 == "PEDRA"){
 							jogadores[indice2].pontuacao += 3;
+							$("#historico").append('<p class="resultado">Jogador 2 ganhou.<br>'+
+								j1.nome+'	vs	'+j2.nome+'<br>'+
+								resultado1+' < '+resultado2+'</p>');
 							return 2;
 						}
 						else{
 							jogadores[indice1].pontuacao += 3;
+							$("#historico").append('<p class="resultado">Jogador 1 ganhou.<br>'+
+								j1.nome+'	vs	'+j2.nome+'<br>'+
+								resultado1+' > '+resultado2+'</p>');
 							return 1;
 						}
 					}
@@ -191,19 +248,28 @@ function verificarResultado(Jogo, resultado1, resultado2) {
 		}
 	}
 	else{
-		if(Jogo == ModoJogo.PvC){
+		if(modo_atual == ModoJogo.PvC){
 			var jogador1 = verificarJogador(j1.nome);
 			//computador foi criado globalmente
 			var pc = computador.opcaoSelecionada();
 			if(resultado1 == "PEDRA"){
 				if (pc == resultado1){
+					$("#historico").append('<p class="resultado">EMPATE<br>'+
+							j1.nome+'	vs	COMPUTADOR<br>'+
+							resultado1+' == '+pc+'</p>');
 					return 0;
 				}
 				else{
 					if(pc == "PAPEL"){
+						$("#historico").append('<p class="resultado">O Computador ganhou.<br>'+
+							j1.nome+'	vs	COMPUTADOR<br>'+
+							resultado1+' < '+pc+'</p>');
 						return -1;
 					}
 					else{
+						$("#historico").append('<p class="resultado">O Jogador 1 ganhou.<br>'+
+							j1.nome+'	vs	COMPUTADOR<br>'+
+							resultado1+' > '+pc+'</p>');
 						return 1;
 					}
 				}
@@ -211,13 +277,22 @@ function verificarResultado(Jogo, resultado1, resultado2) {
 			else{
 				if(resultado1 == "PAPEL"){
 					if(pc == resultado1){
+						$("#historico").append('<p class="resultado">EMPATE<br>'+
+							j1.nome+'	vs	COMPUTADOR<br>'+
+							resultado1+' == '+pc+'</p>');
 						return 0;
 					}
 					else{
 						if(pc == "PEDRA"){
+							$("#historico").append('<p class="resultado">O Jogador 1 ganhou.<br>'+
+								j1.nome+'	vs	COMPUTADOR<br>'+
+								resultado1+' > '+pc+'</p>');
 							return 1;
 						}
 						else{
+							$("#historico").append('<p class="resultado">O Computador ganhou.<br>'+
+								j1.nome+'	vs	COMPUTADOR<br>'+
+								resultado1+' < '+pc+'</p>');
 							return -1;
 						}
 					}
@@ -225,13 +300,22 @@ function verificarResultado(Jogo, resultado1, resultado2) {
 				else{
 					if (resultado1 == "TESOURA"){
 						if(pc == resultado1){
+							$("#historico").append('<p class="resultado">EMPATE<br>'+
+								j1.nome+'	vs	COMPUTADOR<br>'+
+								resultado1+' == '+pc+'</p>');
 							return 0;
 						}
 						else{
 							if(pc == "PAPEL"){
+								$("#historico").append('<p class="resultado">O Jogador 1 ganhou.<br>'+
+									j1.nome+'	vs	COMPUTADOR<br>'+
+									resultado1+' > '+pc+'</p>');
 								return 1;
 							}
 							else{
+								$("#historico").append('<p class="resultado">O Computador ganhou.<br>'+
+									j1.nome+'	vs	COMPUTADOR<br>'+
+									resultado1+' < '+pc+'</p>');
 								return -1;
 							}
 						}
@@ -243,24 +327,31 @@ function verificarResultado(Jogo, resultado1, resultado2) {
 	}
 }
 
-function InterfaceJogo(Jogo){
+function InterfaceJogo(){
+	$("#paragrafoInfo1").text("Nome: "+j1.nome);
 	var r1, r2;
-	if(Jogo == ModoJogo.PvP){
-		$("#Jogo").attr('style','display: inherit');
-		$("#enviarItemJogador1").click(
+	$("#Jogo").attr('style','display: inherit');
+	if(modo_atual == ModoJogo.PvP){
+		$("#paragrafoInfo2").text("Nome: "+j2.nome);
+		$("#enviarItemJogador1").attr('style','display: inline-block');
+		$("#desistir1").attr('style', 'display: inline-block');
+		$("#desistir2").attr('style', 'display: inline-block');
+		$("#enviarItemJogador1").off().click(
 			function(){
-				
+	
 				$("#jg1").hide();
+				$("#enviarItemJogador1").hide();
 				r1 = $("input[name='escolhaJogo1']:checked").val();
 				$("#jg2").attr('style', 'display: inherit');
-				
-				$("#enviarItemJogador2").click(
+				$("#enviarItemJogador2").attr('style','display: inline-block');
+				$("#enviarItemJogador2").off().click(
 					function(){
 						$("#jg2").hide();
+						$("#enviarItemJogador2").hide();
 						r2 = $("input[name='escolhaJogo2']:checked").val();
-						var r = verificarResultado(Jogo, r1, r2);
-						$("#painel").text(r);
+						var r = verificarResultado(r1, r2);
 						$("#jg1").attr('style', 'display: inherit');
+						$("#enviarItemJogador1").attr('style','display: inline-block');
 					}
 				);
 			}
@@ -268,36 +359,31 @@ function InterfaceJogo(Jogo){
 		
 	}
 	else{
-		if(Jogo == ModoJogo.PvC){
-
+		if(modo_atual == ModoJogo.PvC){
+			$("#desistir1").attr('style', 'display: inline-block');
+			$("#enviarItemJogador1").off().click(
+				function(){
+					r1 = $("input[name='escolhaJogo1']:checked").val();
+					var r = verificarResultado(r1, computador.opcaoSelecionada());
+				}
+			);
 		}
 	}
 
-	
-	
+	$("#desistir1").off().click(
+		function(){
+			$("#jogador2").hide();
+			desistir(1);
+		}
+	);
+	$("#desistir2").off().click(
+		function(){
+			$("#jogador2").hide();
+			desistir(2);
+		}
+	);
 	
 }
 
 
-$(document).ready(
-	function(){
-		$("#VsPessoa").click(
-			function(){
-				$("#TipoJogo").hide();
-				criarCadastro(ModoJogo.PvP)
-			}
-		);
-	}
-)
-
-$(document).ready(
-	function(){
-		$("#VsComputador").click(
-			function(){
-				$("#TipoJogo").hide();
-				criarCadastro(ModoJogo.PvC)
-			}
-		);
-	}
-)
 
